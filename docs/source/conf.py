325990afaf4f4735e82ebfdf516370aed0c81539
Lo-Wang <Svechnikov_11@mail.ru>
2024-05-02 03:54:55 +0500

    test docs with sphinx

---
+++ docs/source/conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'NFTree'
+copyright = '2024, Ilya, Polina, Anton, Anatoly, Alex'
+author = 'Ilya, Polina, Anton, Anatoly, Alex'
+release = 'Cooming soon'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
